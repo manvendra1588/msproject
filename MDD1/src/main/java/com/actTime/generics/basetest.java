b94abb72198b6ed3fa31b5d0b26d3d43763fc0c0
manvendra <manvendra@DESKTOP-M58AKV3>
2020-03-08 11:38:49 +0530

    This is my first project.

---
+++ MDD1/src/main/java/com/actTime/generics/basetest.java
@@ -0,0 +1,16 @@
+package com.actTime.generics;
+
+import org.testng.annotations.Test;
+
+public class basetest 
+{
+	@Test
+	public void test1()
+	{
+		System.out.println("Test 11");
+	}
+	
+	
+	
+	
+}
---
+++ MDD1/src/main/java/com/actiTime/pages/LoginPage.java
@@ -0,0 +1,15 @@
+package com.actiTime.pages;
+
+import org.testng.annotations.Test;
+
+public class LoginPage
+{
+	@Test
+	public void test()
+	{
+		System.out.println("Test 11");
+	}
+	
+	
+	
+}
